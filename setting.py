414a56174c09752991023424f2ed17bbc10ce4f4
chenjf2015103095 <49228687+chenjf2015103095@users.noreply.github.com>
2019-08-15 15:10:29 +0800

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   Author :       陈剑锋
+   Date：         2019-08-13 下午2:53
+   Description :  Dream it possible!
+   
+-------------------------------------------------
+   Change Activity:
+
+-------------------------------------------------
+"""
+###################################
+    #环境变量配置,避免代码的改动#
+###################################
+Model_path='/home/ubutnu/chenjf/classify/frozen_graph.pb'
+Label_path = '/home/ubutnu/chenjf/classify/label.txt'
+Image_file = '/home/ubutnu/work/download_image/'
+SAVEPATH = '/home/ubutnu/下载/wab_ldd/'
+HOST='192.168.120.240'
+PORT='8080'
